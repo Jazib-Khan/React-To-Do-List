67bb5aa759c8f1b87fcc6642e37734f341de398c
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2024-03-11 19:03:58 +0000

    New items can be updated to the todo list state

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
+  const [todos, setTodos] = useState([ // useState is a hook that allows you to have state variables in functional components
+    'Go to the gym',
+    'Eat more fruits and veg',
+    'Pick up the kid from school'
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    setTodos(newTodoList)
+  }
 
   return (
     <>
-      <TodoInput />
-      <TodoList />
+      <TodoInput handleAddTodos={handleAddTodos} />
+      <TodoList todos={todos} />
     </>
   )
 }
--- reactjs-todolist/src/components/TodoList.jsx
+++ reactjs-todolist/src/components/TodoList.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
-
-    let todos = [
-        'Go to the gym',
-        'Eat more fruits and veg',
-        'Pick up the kid from school'
-    ] // An array of todo items
+export default function TodoList(props) {
+    const { todos } = props
 
     return (
         <ul className='main'>
             {todos.map((todo, todoIndex) => {
                 return (
-                    <TodoCard key={todoIndex} todo={todo} > 
+                    <TodoCard {...props} key={todoIndex} index={todoIndex} > 
                         <p>{todo}</p>
                     </TodoCard>
-                        
                 )
             })}
         </ul>
